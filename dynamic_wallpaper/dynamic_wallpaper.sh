a7303e88424ab014f827ab017e4fc3d49a523b6a
Stanley Varga <stanleyvarga1@gmail.com>
2024-08-11 10:01:03 +0200

    🔨 chore: updates install script

--- dynamic_wallpaper/dynamic_wallpaper.sh
+++ dynamic_wallpaper/dynamic_wallpaper.sh
@@ -1,2 +1,7 @@
-echo "Running dynamic_wallpaper.sh..."
-bun dynamic_wallpaper.ts
+echo "🏃‍♂️ Running dynamic_wallpaper.sh..."
+
+# Assign the path to the variable
+path="$HOME/.dotfiles/_automac/dynamic_wallpaper"
+
+# Run the dynamic_wallpaper.ts script using the bun command
+bun "$path/dynamic_wallpaper.ts"
